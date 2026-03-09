a3862567801a5c803f2b8bc655f9ba5c5e106464
Jakub Štolba <stolbja2@fel.cvut.cz>
2026-03-09 18:30:59 +0100

    qr code bug fixed

--- Frontend_ski/src/app/scanner/page.tsx
+++ Frontend_ski/src/app/scanner/page.tsx
@@ -126,6 +126,7 @@ export default function ScannerPage() {
               onClick={() => {
                 setShowScanner(true)
                 setLastScanned('')
+                scannedSetRef.current.clear()
               }}
               className="w-full bg-blue-600 text-white py-3 px-4 rounded-md hover:bg-blue-700 transition-colors font-medium"
             >
